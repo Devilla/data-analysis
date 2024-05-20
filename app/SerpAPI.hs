1911493b62459982a3d505136c6889a481de5c65
devilla <dev.koold@gmail.com>
2024-05-20 20:06:06 +0530

    feat: Update SerpAPI module to include API key in request

--- app/SerpAPI.hs
+++ app/SerpAPI.hs
@@ -21,8 +21,8 @@ getFromSerpApi :: String -> IO (Maybe Value)
 getFromSerpApi query = do
     -- Build the request
     let request = setRequestMethod "GET"
-                $ setRequestQueryString [("engine", Just "google"), ("q", Just $ B.pack query)]
-                $ parseRequest_ apiEndpoint
+            $ setRequestQueryString [("engine", Just "google"), ("q", Just $ B.pack query), ("api_key", Just $ B.pack "4621be6f7962e9a08c9aca475f96b344bd604f31aacf6dea5dc073e21eed3482")]
+            $ parseRequest_ apiEndpoint
 
     -- Send the request and get the response
     response <- httpJSON request
