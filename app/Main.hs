aee4676ea98cd031aed45abbb7a7f78d0e804b64
devilla <dev.koold@gmail.com>
2024-05-20 23:11:18 +0530

    feat: res to output.json in Main.hs

--- app/Main.hs
+++ app/Main.hs
@@ -3,12 +3,18 @@ module Main where
 
 import SerpAPI
 import Prelude hiding (print)
-import Prelude (print)
+import qualified Data.Aeson as Aeson
+import qualified Data.ByteString.Lazy as BL
 
 main :: IO ()
 main = do
-    res <- getFromSerpApi "restaurants in New York"
-    print res
+    res <- getFromSerpApi "fruits in Himachal Pradesh"
+
+    -- Convert res into a JSON ByteString
+    let json = Aeson.encode res
+
+    -- Write the JSON ByteString to a file
+    BL.writeFile "data/output.json" json
 
 
 
