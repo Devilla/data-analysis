4ca135c42f8d404855aa48cc633bfad69b96b29e
devilla <dev.koold@gmail.com>
2024-05-30 09:54:01 +0530

    1 of 1 test suites (1 of 1 test cases) passed.

--- tests/GoogleLLMTest.hs
+++ tests/GoogleLLMTest.hs
@@ -3,6 +3,7 @@
 import Test.Hspec
 import GoogleLLM
 import Data.Aeson
+import Data.Maybe (isJust)
 import Data.ByteString.Lazy (ByteString)
 import qualified Data.ByteString.Lazy as L
 
@@ -11,8 +12,8 @@ main = hspec $ do
   describe "generateContent" $ do
     it "returns a non-empty ByteString" $ do
       result <- generateContent
-      L.length result `shouldSatisfy` (> 0)
+      L.length (L.fromStrict result) `shouldSatisfy` (> 0)
 
     it "returns a valid JSON response" $ do
       result <- generateContent
-      (decode result :: Maybe Value) `shouldSatisfy` isJust
+      (decode (L.fromStrict result) :: Maybe Value) `shouldSatisfy` isJust
