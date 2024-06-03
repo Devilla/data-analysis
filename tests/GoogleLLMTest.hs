a596ed852f6d2d8c923b95dfbebf03f349ed912f
devilla <dev.koold@gmail.com>
2024-06-03 16:51:25 +0530

    ParsePreferences removed

--- tests/GoogleLLMTest.hs
+++ tests/GoogleLLMTest.hs
@@ -10,9 +10,9 @@ main :: IO ()
 main = hspec $ do
   describe "generateContent" $ do
     it "returns a non-empty ByteString" $ do
-      result <- generateContent
+      result <- generateContent "Please suggest me indian food"
       L.length (L.fromStrict result) `shouldSatisfy` (> 0)
 
     it "returns a valid JSON response" $ do
-      result <- generateContent
+      result <- generateContent "Please suggest me a meal"
       (decode (L.fromStrict result) :: Maybe Value) `shouldSatisfy` isJust
