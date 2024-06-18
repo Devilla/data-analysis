e959ac1c398fb495c12b8f28e170987b5fb1a0f6
devilla <dev.koold@gmail.com>
2024-06-18 14:31:13 +0530

    getFoodPreferences temperature fixed

--- tests/MainTest.hs
+++ tests/MainTest.hs
@@ -9,6 +9,8 @@ import GoogleLLM
 import Data.Aeson (encode)
 import qualified Data.ByteString as B
 
+data Location = Location { lat :: Double, lon :: Double } deriving (Eq)
+
 main :: IO ()
 main = hspec spec
 
@@ -16,9 +18,10 @@ spec :: Spec
 spec = do
   -- describe "getWeatherData" $ do
   --   it "returns weather data for a given location" $ do
-  --     let location = "New York"
-  --     weatherData <- getWeatherData
-  --     location weatherData `shouldBe` location
+  --     let expectedWeather = Just (Weather {location = GetWeather.Location {localtime = "2024-05-22 9:02"}, current = Current {temp_c = 22.2}})
+  --     case expectedWeather of
+  --       Just weather -> location weather `shouldBe` location weather
+
 
   -- describe "generateContent" $ do
   --   it "generates content based on meal preferences and weather data" $ do
@@ -33,8 +36,8 @@ spec = do
       cuisines <- getPreferredCuisines time
       cuisines `shouldSatisfy` (not . null)
 
-  -- describe "getFoodPreferences" $ do
-  --   it "returns preferred foods for a given temperature" $ do
-  --     let temperature = 20
-  --     foods <- getFoodPreferences temperature
-  --     foods `shouldSatisfy` (not . null)
+  describe "getFoodPreferences" $ do
+    it "returns preferred foods for a given temperature" $ do
+      let temperature = "Above 25°C"
+      foods <- getFoodPreferences temperature
+      foods `shouldSatisfy` (not . null)
